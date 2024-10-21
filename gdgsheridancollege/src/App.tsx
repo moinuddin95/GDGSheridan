2cf0226848633b35c27aab951bee0c75bf4093b8
Moinuddin Mustufa Shaikh <moinuddinshaikh173@gmail.com>
2024-10-21 00:29:10 -0400

    Refactor favicon link in index.html, update logo.png and added About section

--- gdgsheridancollege/src/App.tsx
+++ gdgsheridancollege/src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar/Navbar";
 import Main from "./components/Main/Main";
 import './App.css';
+import About from "./components/About/About";
 function App() {
   return (
     <div>
       <Navbar />
       <Main />
+      <About />
     </div>
   );
 }
---
+++ gdgsheridancollege/src/components/About/About.tsx
@@ -0,0 +1,21 @@
+import "./About.css";
+
+function About() {
+  return (
+    <section id="About">
+      <h1>About</h1>
+      <h3>
+        Through Google Developers Group Club, members will embark on an
+        ever-evolving journey into the world of mobile and web development.
+        Whether you are a beginner or an experienced developer, the GDG offers a
+        broad selection of resources and networking opportunities to cater to
+        your development needs. Throughout the club's monthly events, workshops,
+        and hackathons, members will have the opportunity to collaborate with
+        like-minded individuals, learn from industry professionals, and take
+        part in hands-on coding initiatives.
+      </h3>
+    </section>
+  );
+}
+
+export default About;
