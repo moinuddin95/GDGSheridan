3e9f983991f8b12e6362e0fe9833f7e5922af29d
moinuddin95 <moinuddinshaikh173@gmail.com>
2025-01-28 13:15:07 -0500

    Added the sidebar skeleton

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -25,22 +25,43 @@ const Navbar = () => {
 
   return (
     <header className={`${visibility ? "show" : ""}`}>
-      <nav>
-        
-        <a href="/"><img src={logo} alt="logo" /></a>
-        <div>
+      <menu>
+        <nav>
+          <a href="/">
+            <img src={logo} alt="logo" />
+          </a>
+          <div>
+            <button onClick={() => handleScrollToComponent("About")}>
+              <span>About</span>
+            </button>
+            <button onClick={() => handleScrollToComponent("Events")}>
+              <span>Events</span>
+            </button>
+            <button id="join">
+              <span>Join</span>
+              <img src={redirect} />
+            </button>
+          </div>
+        </nav>
+      </menu>
+      <div className="sidebar">
+        <button id="tabs">
+          <span></span>
+          <span></span>
+        </button>
+
+        <div className="tabs">
           <button onClick={() => handleScrollToComponent("About")}>
-            <span>About</span> 
+            <span>About</span>
           </button>
           <button onClick={() => handleScrollToComponent("Events")}>
             <span>Events</span>
           </button>
-          <button id="join">
+          <button>
             <span>Join</span>
-            <img src={redirect} />
           </button>
         </div>
-      </nav>
+      </div>
     </header>
   );
 };
