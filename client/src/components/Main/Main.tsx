9ad7480097a4a95752f9a5eb3fe63a08c2853b1b
moinuddin95 <moinuddinshaikh173@gmail.com>
2025-02-27 12:44:00 -0500

    Removed test logs

--- client/src/components/Main/Main.tsx
+++ client/src/components/Main/Main.tsx
@@ -12,7 +12,7 @@ export default function Main() {
       <div id="main-content">
         <div id="title">
           <h1 className="slideAnimate">
-            GDG:
+            Google Developer Groups:
             <Typewriter
               words={["Sheridan College"]}
               loop={false}
